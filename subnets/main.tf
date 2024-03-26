20c6e0a679e8682e5b4b1af40ab42a4ed643e725
umamanasa <umamanasa49@gmail.com>
2024-03-26 16:35:21 +0530

    subnets,rt creation

--- subnets/main.tf
+++ subnets/main.tf
@@ -15,3 +15,10 @@ resource "aws_route_table" "main" {
     Name = each.key
   }
 }
+
+resource "aws_route_table_association" "a" {
+  for_each = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null ), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null ), "id", null)
+
+}
