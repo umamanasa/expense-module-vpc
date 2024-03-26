9869b12f561c4d78d90887aec11188f5306c0c16
umamanasa <umamanasa49@gmail.com>
2024-03-26 16:20:33 +0530

    subnets,rt creation

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,12 +1,17 @@
 resource "aws_subnet" "main" {
   for_each = var.subnets
-  vpc_id     = var.vpc_id
-  cidr_block = each.value["cidr"]
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
   tags = {
     Name = each.key
   }
 }
 
-variable "subnets" {}
-variable "vpc_id" {}
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id   = var.vpc_id
+  tags = {
+    Name = each.key
+  }
+}
