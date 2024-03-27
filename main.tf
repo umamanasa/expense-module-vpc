a3499786363a9bccb1586dcddbabaf3182312c0f
umamanasa <umamanasa49@gmail.com>
2024-03-27 12:05:23 +0530

    igw creation

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "aws_vpc" "main" {
 
 module "subnets" {
   source = "./subnets"
-  for_each = var.subnets
-  subnets = each.value
-  vpc_id = aws_vpc.main.id
+  for_each    = var.subnets
+  subnets     = each.value
+  vpc_id      = aws_vpc.main.id
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -22,6 +22,17 @@ resource "aws_route" "igw" {
   route_table_id            = each.value["id"]
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.igw.id
+}
+
+resource "aws_eip" "ngw" {
+  for_each                  = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  domain                    = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  for_each                  = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id             = lookup(lookup(aws_eip.ngw, each.key, null ), "id", null)
+  subnet_id                 = each.value["id"]
 }
 
 output "subnet" {
