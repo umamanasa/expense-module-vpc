887f3882891f0938033ae3949e4178066f311c23
umamanasa <umamanasa49@gmail.com>
2024-03-27 16:32:32 +0530

    creating instance

--- main.tf
+++ main.tf
@@ -60,3 +60,35 @@ resource "aws_route" "default-vpc-peer-entry" {
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
+
+resource "aws_instance" "main" {
+  instance_type           = "t2.micro"
+  ami                     = "ami-0f3c7d07486cad139"
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+  subnet_id               = local.app_subnet_ids[0]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
