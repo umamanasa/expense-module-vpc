9215269c03330da233e65f80c0e4cb05ec49d65c
umamanasa <umamanasa49@gmail.com>
2024-03-27 17:27:21 +0530

    tags

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
+  tags = merge(local.tags, { name = "${var.env}-vpc"})
 }
 
 module "subnets" {
@@ -7,14 +8,13 @@ module "subnets" {
   for_each    = var.subnets
   subnets     = each.value
   vpc_id      = aws_vpc.main.id
+  tags        = local.tags
+  env         = var.env
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
+  tags = merge(local.tags, { name = "${var.env}-igw"})
 }
 
 resource "aws_route" "igw" {
@@ -33,6 +33,7 @@ resource "aws_nat_gateway" "ngw" {
   count                     = length(local.public_subnet_ids)
   allocation_id             = element(aws_eip.ngw.*.id, count.index)
   subnet_id                 = element(local.public_subnet_ids, count.index)
+  tags                      = merge(local.tags, { name = "${var.env}-ngw"})
 }
 
 resource "aws_route" "ngw" {
@@ -46,6 +47,7 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
   auto_accept   = true
+  tags = merge(local.tags, { name = "${var.env}-peer"})
 }
 
 resource "aws_route" "peer" {
@@ -61,34 +63,35 @@ resource "aws_route" "default-vpc-peer-entry" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-resource "aws_instance" "main" {
-  instance_type           = "t2.micro"
-  ami                     = "ami-0f3c7d07486cad139"
-  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
-  subnet_id               = local.app_subnet_ids[0]
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  tags = {
-    Name = "allow_tls"
-  }
-}
+# To launch an instance
+#resource "aws_instance" "main" {
+#  instance_type           = "t2.micro"
+#  ami                     = "ami-0f3c7d07486cad139"
+#  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+#  subnet_id               = local.app_subnet_ids[0]
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id      = aws_vpc.main.id
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 22
+#    to_port          = 22
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
