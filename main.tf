17cf87746c55932af4b8480a66b65c9579fc7da2
umamanasa <umamanasa49@gmail.com>
2024-03-27 15:04:44 +0530

    peering

--- main.tf
+++ main.tf
@@ -42,7 +42,8 @@ resource "aws_route" "ngw" {
   nat_gateway_id            = element(aws_nat_gateway.ngw.*.id, count.index)
 }
 
-output "subnet" {
-  value = module.subnets
+resource "aws_vpc_peering_connection" "peering" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.default_vpc_id
+  auto_accept   = true
 }
-
