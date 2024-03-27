70b8222fa2815908704349befa1fa260316af289
umamanasa <umamanasa49@gmail.com>
2024-03-27 17:34:20 +0530

    tags

--- subnets/main.tf
+++ subnets/main.tf
@@ -16,5 +16,4 @@ resource "aws_route_table_association" "a" {
   for_each = var.subnets
   subnet_id      = lookup(lookup(aws_subnet.main, each.key, null ), "id", null)
   route_table_id = lookup(lookup(aws_route_table.main, each.key, null ), "id", null)
-
 }
